6190183c75a447e4d5d965322204f9da3f7c28d5
scottkoons <scottkoons@gmail.com>
2020-09-28 14:28:52 -0600

    final

--- app.js
+++ app.js
@@ -124,4 +124,124 @@ function valTimesIndex(arr) {
   });
 }
 
-console.log(valTimesIndex([1, -2, -3]));
+// console.log(valTimesIndex([1, -2, -3]));
+
+// ----------------------------------------
+
+// -------------- extractKey ---------------
+// Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value of that key in each object.
+
+function extractFullName(arr) {
+  return arr.map(function (el) {
+    return `${el.first} ${el.last}`;
+  });
+}
+
+// console.log(
+//   extractFullName([
+//     { first: "Scott", last: "Koons" },
+//     { first: "Pablo", last: "Napp" },
+//     { first: "Don", last: "Know" },
+//     { first: "Colt", last: "Steele" },
+//   ])
+// );
+
+// ----------------------------------------
+
+// -------------- filterByValue ---------------
+// Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
+
+function filterByValue(arr, key) {
+  return arr.filter(function (arr) {
+    return arr.isCatOwner;
+  });
+}
+
+const catOwner = filterByValue(
+  [
+    { first: "Elie", last: "Schoppik" },
+    { first: "Tim", last: "Garcia", isCatOwner: true },
+    { first: "Matt", last: "Lane" },
+    { first: "Colt", last: "Steele", isCatOwner: true },
+  ],
+  "isCatOwner"
+);
+// console.log(catOwner);
+
+// ----------------------------------------
+
+// ----------------- find -----------------
+// Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
+
+function find(arr, val) {
+  const checkNum = val;
+  return arr.filter(function (arr) {
+    if (arr === checkNum) {
+      return arr;
+    }
+  })[0];
+}
+
+// console.log(find([1, 2, 3, 4, 5], 10));
+// ----------------------------------------
+
+// ----------------- findInObj -----------------
+// Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
+
+function findInObj(arr, key, val) {
+  return arr.filter(function (el) {
+    return el[key] === val;
+  })[0];
+}
+
+const returnVal = findInObj(
+  [
+    { first: "Elie", last: "Schoppik" },
+    { first: "Tim", last: "Garcia", isCatOwner: true },
+    { first: "att", last: "Lane" },
+    { first: "Colt", last: "Steele", isCatOwner: true },
+  ],
+  "isCatOwner",
+  true
+);
+// console.log(returnVal);
+// {first: 'Tim', last:"Garcia", isCatOwner: true}
+// ----------------------------------------
+
+// ----------------- removeVowels -----------------
+// Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
+
+function removeVowels(str) {
+  const vowels = "aeiou";
+  const lowerCase = str.toLowerCase();
+  const splitStr = lowerCase.split("");
+  return splitStr
+    .filter(function (letter) {
+      let noVowel = vowels.indexOf(letter) < 0;
+
+      return noVowel;
+    })
+    .join(" ");
+}
+
+// console.log(removeVowels("Ellie"));
+// console.log(removeVowels("TIM"));
+// console.log(removeVowels("ZZZZZZ"));
+// ----------------------------------------
+
+// ----------------- doubleOddNumbers -----------------
+// Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
+
+function doubleOddNumbers(arr) {
+  return arr
+    .filter(function (val) {
+      return val % 2 !== 0;
+    })
+    .map(function (val) {
+      return val * 2;
+    });
+}
+
+console.log(doubleOddNumbers([1, 2, 3, 4, 5]));
+// [2,6,10]
+// doubleOddNumbers([4,4,4,4,4]) // []
